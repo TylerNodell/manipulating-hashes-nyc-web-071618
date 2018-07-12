62e612576ed55a0981912a7406e1f64f7bf6096f
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 15:17:44 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts.each { |person, data|
     data.each { |key, value|
       if key == :favorite_icecream_flavors && value.include?("strawberry")
-        strawberry = value.indexOf("strawberry")
+        strawberry = value.index("strawberry")
         value.delete(strawberry)
       end
     }
